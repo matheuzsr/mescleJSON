11e68d239aad12d34204167e3f6164cc2405a3c1
matheuzs <matheusdasilvarocha@gmail.com>
2020-03-31 04:13:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+
+const infos = require('./merge.json');
+const municipios = require('./municipios.json');
+
+const infoFull = [];
+/**
+ * const munisES= municipios.filter(( elem, index, array)=>{ 
+ * if (elem["codigo_uf"]==31){
+ *   return elem;
+ * }
+ * }) 
+ * 
+ */
+
+infos.map(info => {
+
+  municipios.map(muni => {
+
+    if (info.municipio_residencia == muni.nome) {
+      const {latitude, longitude} = muni;
+      const { data, boletim, municipio_residencia, caso_confirmado, caso_descartado, caso_suspeito, mortes } = info;
+      infoFull.push ({
+        data,
+        boletim,
+        municipio_residencia,
+        caso_confirmado,
+        caso_descartado,
+        caso_suspeito,
+        mortes,
+        latitude,
+        longitude
+      })
+    }
+  })
+
+})
+
+
+fs.writeFile("/home/matheus/www/data-es/myjsonfull.json", JSON.stringify(infoFull), function(erro) {
+
+  if(erro) {
+      throw erro;
+  }
+
+  console.log("Arquivo salvo");
+}); 
